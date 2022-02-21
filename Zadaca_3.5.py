79ccfda16d78bcc353456fe2ab7a0865c6dddff3
maja-matanovic <maja.matanovic@infinum.com>
2022-02-21 07:17:09 +0100

    Added Task 3.5

---
+++ Zadaca_3.5.py
@@ -0,0 +1,26 @@
+lista = []
+
+
+def automehanicar(ime):
+    print(ime)
+    remen = float(input("Popravak zupčastog remena: ").strip(" kn"))
+    branik = float(input("Popravak branika: ").strip(" kn"))
+    popust = float(input("Popust: ").strip(" %"))
+    suma = (remen + branik) - (popust / 100) * (remen + branik)
+    print("Ukupno: " + str(suma))
+    return suma
+
+
+a = automehanicar("Renato")
+lista.append(a)
+b = automehanicar("Marino")
+lista.append(b)
+c = automehanicar("Luka")
+lista.append(c)
+
+if min(lista) == a:
+    print("Najbolja opcija je Renato: " + str(a) + " kn")
+if min(lista) == b:
+    print("Najbolja opcija je Marino: " + str(b) + " kn")
+if min(lista) == c:
+    print("Najbolja opcija je Luka: " + str(c) + " kn")
